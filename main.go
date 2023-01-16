66b85e9c3c1500ef9b8ac6fe94f6ab8b503beb56
Artem Oliinyk <azure.email@gmail.com>
2023-01-16 18:54:40 +0100

    some visual stuff

--- main.go
+++ main.go
@@ -4,28 +4,50 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+
+	// "math/rand"
 	"time"
+
+	screenHelper "github.com/artemoliynyk/go-find-the-way/screen"
+
+	"github.com/gdamore/tcell/v2"
 )
 
 const DEFAULT_MAP_SIZE = 10
 
-func main() {
+var screen tcell.Screen;
+var style tcell.Style;
+
+func printIntro(seed int64, size int) {
 	fmt.Println("Let's find the way out here!")
 	fmt.Println("")
 	fmt.Println("Want a same map? Just give a seed like this: ./go-find-the-way -seed=5675435675")
 	fmt.Println("Adjust the map size, like 15x15: ./go-find-the-way --size=15")
 	fmt.Println("")
+	fmt.Printf("Current seed: %d\n", seed)
+	fmt.Printf("Map size: %d\n", size)
+	fmt.Println("")
+}
 
+func main() {
+
+	quiet := flag.Bool("q", false, "No into text")
 	mapSize := flag.Int("size", DEFAULT_MAP_SIZE, "Map size n*n")
-	seed := flag.Int64("seed", time.Now().Unix(), "Map seed")
+	mapSeed := flag.Int64("seed", time.Now().Unix(), "Map seed")
 	flag.Parse()
 
-	fmt.Printf("Current seed: %d\n", *seed)
-	rand.Seed(*seed)
+	if !*quiet {
+		printIntro(*mapSeed, *mapSize)
+	}
 
-	fmt.Printf("Map size: %d\n", *mapSize)
+	screen, style = screenHelper.InitScreen()
+
+	// seed our map
+	rand.Seed(*mapSeed)
+
+	// make map
 	area := make([][]int, *mapSize)
-
 	for row := 0; row < *mapSize; row++ {
 		area[row] = make([]int, *mapSize)
 
@@ -36,5 +58,29 @@ func main() {
 
 	for row := 0; row < *mapSize; row++ {
 		fmt.Println(area[row])
+	}
+
+	screenHelper.DrawText(screen, 0, 0, 10, 10, style, "Test")
+
+	quit := func() {
+		screen.Fini()
+		os.Exit(0)
+	}
+	for {
+		// Update screen
+		screen.Show()
+	
+		// Poll event
+		ev := screen.PollEvent()
+	
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			}
+		}
 	}
 }
